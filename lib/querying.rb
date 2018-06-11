e2903720d6051760fb1e6d2a119dca077ec3e803
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-11 06:00:59 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -44,5 +44,5 @@ def select_character_names_and_number_of_books_they_are_in
   JOIN characters
   ON character_books.character_id = characters.id
   GROUP BY character_books.character_id
-  ORDER BY appearance_count DESC;"
+  GROUP BY characters.name;"
 end
